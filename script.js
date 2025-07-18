0cc038fd6f4801f4d038248af3ea14d08e0f86e8
Mothish_bk <49220593+Mothishbk@users.noreply.github.com>
2025-07-18 15:49:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggles = document.querySelectorAll(".accordion-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    const content = toggle.nextElementSibling;
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+});
